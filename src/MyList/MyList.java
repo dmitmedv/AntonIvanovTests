934c077c539769aafe3ca83d97de34e24dea38dd
antonqwerty <Fynjy199388>
2020-10-24 15:02:47 +0300

    Add classic array printing

--- src/MyList/MyList.java
+++ src/MyList/MyList.java
@@ -12,12 +12,22 @@ public class MyList {
         elements[size] = integer;
         size++;
     }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
+        String start = "[";
+        String end = "]";
+        String delimetr = "; ";
+        sb.append(start);
         for (int i = 0; i < size; i++) {
-            sb.append(elements[i] + "\n");
+            if (i != elements.length - 1) {
+                sb.append(elements[i]).append(delimetr);
+            } else if (i == elements.length - 1) {
+                sb.append(elements[i]);
+            }
         }
+        sb.append(end);
         return sb.toString();
     }
 }
